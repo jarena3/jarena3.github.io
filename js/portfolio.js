70b747be2780bfd213d1a8d2b89d468617a11bcc
John Arena <john.arena.3@gmail.com>
2015-04-09 02:23:47 +0900

    color change

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+$(document).ready(function ( ){
+    console.log("!");
+      $('#bbtn').on('click', function () {
+        console.log("!!!");
+        var container = document.getElementById('content');
+        console.log(container);
+        $('#content').load('html/about.html');
+  })
+
+})
