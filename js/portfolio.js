7105ed4c1899933d04ef2a964555646d5d02b77b
John Arena <john.arena.3@gmail.com>
2015-04-10 01:02:01 +0900

    js fixes

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,15 +1,22 @@
 // JavaScript Document
-$(document).ready(function ( ){
+$(document).ready(function ( )
+    {
     $('#content').load('html/about.html');
-
-      $('#aboutBTN').on('click', function () {
-        $('#content').load('html/about.html');
-	  $('#resumeBTN').on('click', function () {
-        $('#content').load('html/resume.html');
-	  $('#projectsBTN').on('click', function () {
-        $('#content').load('html/projects.html');
-	  $('#contactBTN').on('click', function () {
-        $('#content').load('html/contact.html');
-  })
-
-})
+    
+    $('#aboutBTN').on('click', function () 
+        {
+            $('#content').load('html/about.html');
+        });
+	$('#resumeBTN').on('click', function () 
+        {
+            $('#content').load('html/resume.html');
+        });
+	$('#projectsBTN').on('click', function () 
+        {
+            $('#content').load('html/projects.html');
+        });
+	$('#contactBTN').on('click', function () 
+        {
+            $('#content').load('html/contact.html');
+        });
+    });
