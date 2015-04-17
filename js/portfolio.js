36017242a4023324f84fdf0910d7ca1bcb270813
John Arena <john.arena.3@gmail.com>
2015-04-18 01:15:44 +0900

    restructuring portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,22 +1,23 @@
 // JavaScript Document
-$(document).ready(function ( )
-    {
-    $('#content').load('html/about.html');
+
+$(document).ready(function () {
     
-    $('#aboutBTN').on('click', function () 
-        {
-            $('#content').load('html/about.html');
-        });
-	$('#resumeBTN').on('click', function () 
-        {
-            $('#content').load('html/resume.html');
-        });
-	$('#projectsBTN').on('click', function () 
-        {
-            $('#content').load('html/projects.html');
-        });
-	$('#contactBTN').on('click', function () 
-        {
-            $('#content').load('html/contact.html');
-        });
+    "use strict";
+    
+    $('#content').load('html/about.html');
+
+    $('#aboutBTN').on('click', function () {
+        $('#content').load('html/about.html');
     });
+    $('#resumeBTN').on('click', function () {
+        $('#content').load('html/resume.html');
+    });
+    $('#projectsBTN').on('click', function () {
+        $('#content').load('html/projects.html');
+    });
+    $('#contactBTN').on('click', function () {
+        $('#content').load('html/contact.html');
+    });
+    
+
+});
